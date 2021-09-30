9578d294f34eabfd280c6ad3526488708d67c211
Vitor Veras <vitorverasm@gmail.com>
2021-09-30 11:58:30 -0300

    fix: fix components imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Bio from "../components/bio"
 import Button from "../components/button"
+import Layout from "../components/layout"
+import SearchPosts from "../components/searchPosts"
+import SEO from "../components/seo"
 
 class IndexPage extends React.Component {
   render() {
